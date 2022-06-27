4c9a15c6fa9540a2d762cc1eb633812012db57c5
lutfiqaraman <lutfiqaraman@gmail.com>
2022-06-27 10:55:59 +0300

    build product details

--- src/app/shared/product.service.ts
+++ src/app/shared/product.service.ts
@@ -1,50 +1,52 @@
-import { Injectable } from '@angular/core';
-import { Product } from "../entities/product";
-
-@Injectable({
-  providedIn: 'root'
-})
-export class ProductService {
-
-  products: Product[] = [];
-
-  constructor() { }
-
-  getProducts() {
-    this.products = [
-      {
-        id: 1,
-        title: 'First Product',
-        price: 24.99,
-        rating: 4.3,
-        description: 'Product No. 0, a product 0 is a product to be used for hardware usages',
-        categories: [
-          'electronics', 'hardware'
-        ]
-      },
-
-      {
-        id: 2,
-        title: 'Second Product',
-        price: 64.99,
-        rating: 3.5,
-        description: 'Product No. 1, a product 1 is a product to be used for book readers',
-        categories: ['books']
-      },
-      {
-        id: 3,
-        title: 'Third Product',
-        price: 21.99,
-        rating: 3.5,
-        description: 'Product No.3, a product 3 is a product to be used for food consumers',
-        categories: ['books']
-      }
-    ];
-
-    return this.products;
-  }
-
-  getProductById(productID: number) {
-    return this.products.find(p => p.id == productID);
+export class Product {
+  constructor(
+    public id: number,
+    public title: string,
+    public price: number,
+    public rating: number,
+    public description: string,
+    public categories: string[])
+  {
   }
 }
+
+export class ProductService {
+
+  getProducts(): Product[] {
+    return products.map(p => new Product(p.id, p.title, p.price, p.rating, p.description, p.categories));
+  }
+
+  getProductById(productId: number) {
+    return products.find(p => p.id === productId);
+  }
+
+}
+
+const products = [
+  {
+    id: 1,
+    title: 'First Product',
+    price: 24.99,
+    rating: 4.3,
+    description: 'Product No. 1, a product 1 is a product to be used by hardware lovers',
+    categories: [
+      'electronics', 'hardware'
+    ]
+  },
+  {
+    id: 2,
+    title: 'Second Product',
+    price: 64.99,
+    rating: 3.5,
+    description: 'Product No. 2, a product 2 is a product to be used by book readers',
+    categories: ['books']
+  },
+  {
+    id: 3,
+    title: 'Third Product',
+    price: 21.99,
+    rating: 3.5,
+    description: 'Product No.3, a product 3 is a product to be used by food consumers',
+    categories: ['books']
+  }
+];
